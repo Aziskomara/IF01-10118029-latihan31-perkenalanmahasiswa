8b73838daa2ddb1734d8a856c43f9d7ea20088d2
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-10-17 12:36:58 +0700

    Add files via upload

---
+++ IF01-10118029-latihan31-perkenalanmahasiswa/src/if01/pkg10118029/latihan31/perkenalanmahasiswa/IF0110118029Latihan31Perkenalanmahasiswa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118029.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author acer
+ */
+public class IF0110118029Latihan31Perkenalanmahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         mahasiswa mahasiswa = new mahasiswa();
+           
+            
+            
+            mahasiswa. nim = "10118029";
+            mahasiswa.nama ="Azis komara";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118028";
+            mahasiswa.nama = "Surya";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118012";
+            mahasiswa.nama = "moc rizki maulana";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118008";
+            mahasiswa.nama = "mimel";
+            mahasiswa.perkenalanDiri();
+    }
+    
+}
